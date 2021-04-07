72d6a47e25c333941413cc493742d0713aff29d2
devilzcore <aldosilva1@live.com>
2021-04-07 17:36:41 -0300

    Add simples API consume

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Axios from 'axios'
+import {
+  Paper,
+  TextField,
+  FormControl,
+  Select,
+  Button
+} from "@material-ui/core";
 
-function App() {
+import "./styles.css";
+
+const App = () => {
+  const [ text1, setText1 ] = useState(1)
+  const [ text2, setText2 ] = useState(1)
+  const [ country, setCountry ] = useState([])
+  const [ country2, setCountry2 ] = useState([]) 
+  const [ value1, setValue1 ] = useState(1)
+  const [ value2, setValue2 ] = useState(1)
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  async function getData() {
+    const result = await Axios.get("http://data.fixer.io/api/latest?access_key=8cfc561aaf24e3aa13495f20de4358a5")
+    setCountry(result.data.rates)
+    setCountry2(result.data.rates)
+  }
+
+  function convert(e) {
+    e.preventDefault()
+    let num = (value2 / value1) * text1
+    setText2(num)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Paper className="paper">
+        <h3>Currency Converter</h3>
+        <form action="" onSubmit={convert}>
+          <div>
+            <TextField variant="outlined" value={text1 || ""} onChange={(e)=>setText1(e.target.value)} autoComplete="off" />
+            <FormControl className="dropdown" variant="outlined" onChange={(e)=>setValue1(e.target.value)}>
+              <Select native>
+                {Object.keys(country).map((value, index) => <option key={index} value={country[value]}>{value}</option>)}
+              </Select>
+            </FormControl>
+          </div>
+          <div>
+            <TextField variant="outlined" value={text2 || ""}/>
+            <FormControl className="dropdown" variant="outlined" onChange={(e)=>setValue2(e.target.value)}>
+              <Select native>
+                {Object.keys(country2).map((value, index) => <option key={index} value={country[value]}>{value}</option>)}
+              </Select>
+            </FormControl>
+          </div>
+          <Button type="submit"  className="button" variant="contained">
+            Convert
+          </Button>
+        </form>
+      </Paper>
     </div>
   );
-}
+};
 
 export default App;
